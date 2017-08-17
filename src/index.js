66f7127766254012e628724e6567225df173577f
Mitesh Shah <mitesh@mokko.io>
2017-08-18 00:52:03 +0530

    Theme itself is exported as well.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import SideBar from './containers/SideBar';
 import Padded from './containers/Padded';
 import MainPanel from './containers/MainPanel';
 
+import theme from './theme';
+
 module.exports = {
   Button,
   Card,
   SideBar,
   Padded,
   MainPanel,
-  NavLink
+  NavLink,
+
+
+  theme
 };
