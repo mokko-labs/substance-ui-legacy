187a36d1c32cc59e9301736c5016ce76955576a6
Mitesh Shah <mitesh@mokko.io>
2017-08-24 15:06:05 +0530

    Sidebar allows specifying width as a prop.

--- src/containers/SideBar.js
+++ src/containers/SideBar.js
@@ -4,7 +4,7 @@ import theme from '../theme';
 const SideBar = styled.div`
   background: ${theme.sidebar.bg};
   min-width: 64px;
-  width: 200px;
+  width: ${props => props.width || '240px'};
   position: fixed;
   top: 0; left: 0; bottom: 0;
   box-shadow: 0px 0px 8px #DDD;
