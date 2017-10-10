5563c82f234dc0402b086530851c578be24c71c8
Devang Patel <devang@mokko.io>
2017-10-10 14:25:20 +0530

    Added input field component support with multiple parameters

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // ES6
-import styled, { withTheme } from 'styled-components';
+import styled, {withTheme} from 'styled-components';
 
-
-const Input = styled.input`
+const Input = styled.input `
 	padding: 1em;
   border: 1px solid #C2C2C2;
   display: block;
@@ -11,77 +10,150 @@ const Input = styled.input`
   font-size: 0.9em;
   box-sizing: border-box;
 	border-radius: 3px;
+	outline: 0;
+	color: ${props => props.theme.colors.heading
+  ? props.theme.colors.heading
+  : '#000'};
+
+	&.error,
+	&.error:focus {
+		border-color: ${props => props.theme.colors.error };
+	}
+
+	&.success,
+	&.success:focus {
+		border-color: ${props => props.theme.colors.success };
+	}
+
+	&:focus {
+		border-color:${props => props.theme.colors.primary
+    ? props.theme.colors.primary
+    : '#000'};
+	}
 
   ::-webkit-input-placeholder {
-    color:#ccc;
+    color:#afafaf;
   }
   :-moz-placeholder           {
-    color:#ccc;
+    color:#afafaf;
   }
   ::-moz-placeholder          {
-    color:#ccc;
+    color:#afafaf;
   }
   :-ms-input-placeholder      {
-    color:#ccc;
+    color:#afafaf;
   }
 `;
 
-const LabelWrap = styled.div`
+const IconWrap = styled.span `
+	display: inline-block;
+	position: absolute;
+	left:10px;
+	top:0;
+	top:50%;
+	transform: translate(0, -50%);
+
+	&>* {
+		width: 1.1em;
+		height: 1em;
+		opacity: 0.5;
+		font-size: 1.2em;
+	}
+`;
+
+const InputWrap = styled.div `
+	position: relative;
+
+
+	input {
+		padding-left: ${props => props.icon
+		? '40px'
+		: '1em'};
+	}
+
   label {
     font-weight: 400;
     font-size: 1em;
     margin-bottom: 7px;
+		pointer-events: none;
     display: block;
+		display: block;
+		position: absolute;
+		font-size: ${props => props.icon
+	  ? '0.9em'
+	  : '1em'};
+		left: ${props => {
+			if(props.icon) {
+				return '40px'
+			} else if (props.floating) {
+				return '1em'
+			} else {
+				return '0'
+			};
+		}};
+
+		top:${props => props.floating
+    ? '50%'
+    : 0};
+		color:${props => props.floating
+    ? '#afafaf'
+    : '#000'};
+		transform: ${props => props.floating
+    ? 'translate(0, -50%)'
+    : 'translate(0, -120%)'};
+		transition: 300ms all ease-out;
   }
+
+	input:focus + label,
+	&.hasValue input + label {
+		top:0;
+		left:0;
+		opacity: 1;
+		color: ${props => props.floating
+    ? props.theme.colors.primary
+    : '#000'};
+		padding-left: 0;
+		font-size: 1em;
+		transform: translate(0, -120%);
+	}
 `;
 
-const FloatingLabelWrap = styled.div`
-  label {
-    font-weight: 400;
-    font-size: 1em;
-    margin-bottom: 7px;
-    display: block;
+class TextInput extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+    };
+
   }
-`;
 
-
-const TextInput = (props) => {
-
-  if (props.label) {
+  render() {
     return (
-      <LabelWrap>
-        <label>{props.label}</label>
-        <Input
-          placeholder={props.placeholder}
-          type={props.type}
-        />
-      </LabelWrap>
+      <InputWrap error={this.props.error} className={this.props.value
+        ? 'hasValue'
+        : 'hasNoValue'} icon={this.props.icon
+        ? true
+        : false} floating={this.props.floating}>
+        {this.props.icon
+          ? <IconWrap>{this.props.icon}</IconWrap>
+          : null}
+        <Input className={this.props.error ? 'error' : this.props.success ? 'success' : ''} value={this.props.value} onChange={this.props.onChange} placeholder={this.props.placeholder
+          ? this.props.placeholder
+          : ''} type={this.props.type}/> {this.props.label
+          ? <label>{this.props.label}</label>
+          : null}
+      </InputWrap>
     );
   }
 
-  if (props.floating && props.label) {
-    return (
-      <FloatingLabelWrap>
-        <label>{props.label}</label>
-        <Input
-          placeholder={props.placeholder}
-          type={props.type}
-        />
-      </FloatingLabelWrap>
-    );
-  }
-
-  return <Input
-    placeholder={props.placeholder}
-    type={props.type}
-  />
 };
 
 TextInput.propTypes = {
+  floating: PropTypes.bool,
+  icon: PropTypes.node,
   label: PropTypes.string,
-  floating: PropTypes.bool
+  placeholder: PropTypes.string,
+  type: PropTypes.string
 };
-
-
 
 export default withTheme(TextInput);
