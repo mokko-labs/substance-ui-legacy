26153a4d55e72b4dc6f9b28d018f299d2dc311d7
Pritish Vaidya <pritishvaidya94@gmail.com>
2017-11-02 18:08:29 +0530

    Adding the props test cases for area charts

---
+++ src/tests/Charts/AreaChart.spec.js
@@ -0,0 +1,49 @@
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import {AreaChart} from '../../components/Charts/index'
+
+import { mount, configure } from 'enzyme';
+
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+test('Area Chart Styled Components', () => {
+  const wrapper = mount(
+    <AreaChart chartStyle={{fill:'white'}}
+               height={250}
+               animation={{
+                 duration: 2000,
+                 onLoad: {
+                   duration: 1000
+                 }
+               }}
+               data={[
+                 { x: 1, y: 2},
+                 { x: 2, y: 3},
+                 { x: 3, y: 5},
+                 { x: 4, y: 4},
+                 { x: 5, y: 6}
+               ]}
+    />
+  );
+    // Match the props
+  expect(wrapper.props().chartStyle).toEqual({fill:'white'})
+  expect(wrapper.props().height).toEqual(250)
+  expect(wrapper.props().animation).toEqual({
+    duration: 2000,
+    onLoad: {
+      duration: 1000
+    }
+  })
+  expect(wrapper.props().data).toEqual([
+    { x: 1, y: 2},
+    { x: 2, y: 3},
+    { x: 3, y: 5},
+    { x: 4, y: 4},
+    { x: 5, y: 6}
+  ])
+
+})
